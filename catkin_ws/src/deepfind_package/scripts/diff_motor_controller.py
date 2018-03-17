9fd1149133b715600b4e658b1f12f9fe8840fe3d
giovanni2694 <giovanni.gardon@upr.edu>
2018-03-17 17:42:24 -0400

    Casted command variables to integer

--- catkin_ws/src/deepfind_package/scripts/diff_motor_controller.py
+++ catkin_ws/src/deepfind_package/scripts/diff_motor_controller.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import rospy
-#from std_msgs.msg import *
 from deepfind_package.msg import motor_command
 from geometry_msgs.msg import Twist
 
@@ -10,8 +9,9 @@ dict = {'MOTOR_CAP': 40, 'FOWARD': 0, 'BACKWARD': 1}
 
 
 def diffControlCallback(twistData):
-  command.leftMotorPower = (twistData.linerar.x + twistData.angualr.z) * dict['MOTOR_CAP']
-  command.rightMotorPower = (twistData.linerar.x - twistData.angualr.z) * dict['MOTOR_CAP']
+  command = motor_command()
+  command.leftMotorPower = int((twistData.linear.x - twistData.angular.z) * dict['MOTOR_CAP'])
+  command.rightMotorPower = int((twistData.linear.x + twistData.angular.z) * dict['MOTOR_CAP'])
 
   if command.leftMotorPower > 0:
   	command.leftMotorDirection = dict['FOWARD']
@@ -21,17 +21,22 @@ def diffControlCallback(twistData):
   if command.rightMotorPower > 0:
     command.rightMotorDirection = dict['FOWARD']
   else:
-  	motor2Direction = dict['BACKWARD']
+    command.rightMotorDirection = dict['BACKWARD']
 
+  command.leftMotorPower = abs(command.leftMotorPower)
+  command.rightMotorPower = abs(command.rightMotorPower)
  	
   pub = rospy.Publisher('motor_speed', motor_command, queue_size = 10)
   pub.publish(command)
 
 
 def diffControllerListener():
-	rospy.init_node('android_listener')
-	rospy.Subscriber('cmd_vel', Twist, diffControlCallback)
-	rospy.spin()
+  rospy.init_node('android_listener')
+  rospy.Subscriber('cmd_vel', Twist, diffControlCallback)
+  rospy.spin()
 
 if __name__ == '__main__':
     diffControllerListener()
+
+#export ROS_MASTER_URI=http://192.168.43.212:11311
+
