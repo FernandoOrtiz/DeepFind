8436bcc89b38a26f5fbe9379a4c25003c1b752a0
ortizfer <Sic@rio21>
2018-03-31 19:51:48 -0400

    ended real time test

--- test_codes/real_time_test.py
+++ test_codes/real_time_test.py
@@ -1,26 +1,31 @@
 import datetime
+from catkin_ws.src.deepfind_package.scripts import SystemCom
 
+
+SystemCom.init_connections()
 test_result = False
-start = datetime.datetime.now().time()/100
+
+#NOT NEEDED AT THE MOMENT
+#start = datetime.datetime.now().time()/100
 
 #ROS send and receive data
 
-end = datetime.datetime.now().time()/100
+#end = datetime.datetime.now().time()/100
 
 
-elapsed_time = end - start
+elapsed_time = 0
 
 if elapsed_time < 0.05:
     test_result = True
-    print("ROS real time test Pass:")
-    print("Expected time < than 0.05 seconds:")
-    print("Result: "+str(elapsed_time))
+    #print("ROS real time test Pass:")
+    #print("Expected time < than 0.05 seconds:")
+    #print("Result: "+str(elapsed_time))
 
 if test_result:
     test_result = False
     start = datetime.datetime.now().time() / 100
 
-    #Gather Lidar data
+    SystemCom.getSensorData()
 
     end = datetime.datetime.now().time() / 100
 
@@ -36,7 +41,7 @@ if test_result:
     test_result = False
     start = datetime.datetime.now().time() / 100
 
-    #Gather IMU data
+    SystemCom.getSensorData()
 
     end = datetime.datetime.now().time() / 100
 
