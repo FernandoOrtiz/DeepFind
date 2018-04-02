dd0df29eeb7dfb38b0de69c0f59135c6e8c5109f
giovanni2694 <giovanni.gardon@upr.edu>
2018-04-02 22:45:38 +0000

    Updated SLAM launch file

--- catkin_ws/src/deepfind_package/scripts/SystemCom.py
+++ catkin_ws/src/deepfind_package/scripts/SystemCom.py
@@ -3,15 +3,36 @@
 import rospy
 from std_msgs.msg import *
 from deepfind_package.msg import *
-from sensor_msgs.msg import LaserScan, Imu
+from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
+
+sensorData = sensor_data()
+
+#-----------------------Callback Functions-------------------#
+
+def imu_callback(data):
+	sensorData.imu = data
+#	print(sensorData.imu)
+
+def lidar_callback(data):
+	sensorData.lidar = data
+
+def encoder_callback(data):
+	sensorData.encoder = data
 
 
 #----------------------Communication Set Up-----------------#
-def init_connections():
+def run():
+	global sensorData
+	sensorData = sensor_data()
 	rospy.init_node('system_communication')
 	global controlPub
 	controlPub = rospy.Publisher('cmd_vel', Twist, queue_size = 10)
+	imuSub = rospy.Subscriber('vn100_yaw', imu_data, imu_callback)
+	lidarSub = rospy.Subscriber('scan', LaserScan, lidar_callback)
+	encoderSub = rospy.Subscriber('encoder', encoders_data, encoder_callback)
+	print(sensorData)
+	rospy.spin()
 
 #----------------------Publisher----------------------------#
 
@@ -21,12 +42,8 @@ def send_commnand(command):
 
 #----------------------Subscriber---------------------------#
 def get_sensor_data():
-    sensorData = sensor_data()
-    sensorData.imu = rospy.wait_for_message('vn100_yaw', imu_data)
-    sensorData.lidar = rospy.wait_for_message('scan', LaserScan)
-    sensorData.encoder = rospy.wait_for_message('encoder', encoders_data)
-    return sensorData
+	print(sensorData)
+	return sensorData
 
 if __name__ == '__main__':
-    print(get_sensor_data())
-
+	run()
--- real_time_test.py
+++ real_time_test.py
@@ -1,8 +1,10 @@
 import datetime
 from catkin_ws.src.deepfind_package.scripts import SystemCom
+from catkin_ws.src.deepfind_package.msg import *
+import rospy
 
 
-SystemCom.init_connections()
+
 test_result = False
 
 #NOT NEEDED AT THE MOMENT
@@ -19,7 +21,7 @@ if elapsed_time < 0.05:
     test_result = True
     #print("ROS real time test Pass:")
     #print("Expected time < than 0.05 seconds:")
-    #print("Result: "+str(elapsed_time))
+    #print("Result: "+str(elapsed_time) + "microseconds")
 
 if test_result:
     test_result = False
@@ -34,8 +36,8 @@ if test_result:
     if elapsed_time < 50000:
         test_result = True
         print("Test 1 real time test Pass:")
-        print("Expected time < than 50000 micro seconds:")
-        print("Result: " + str(elapsed_time))
+        print("Expected time < than 50000 microseconds:")
+        print("Result: " + str(elapsed_time)+ " microseconds")
     else: 
         print(" Failed Test 1: " + str(elapsed_time))
 if test_result:
@@ -51,8 +53,8 @@ if test_result:
     if elapsed_time < 50000:
         test_result = True
         print("Test 2 real time test Pass:")
-        print("Expected time < than 50000 micro seconds:")
-        print("Result: " + str(elapsed_time))
+        print("Expected time < than 50000 microseconds:")
+        print("Result: " + str(elapsed_time)+ " microseconds")
     else:
         print(" Failed Test 2: " + str(elapsed_time))
 if test_result:
