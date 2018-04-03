b05019324e0386544aead8189fc6e3b082f4c21c
giovanni2694 <giovanni.gardon@upr.edu>
2018-04-03 01:04:42 +0000

    changed SystemCom

--- catkin_ws/src/deepfind_package/scripts/SystemCom.py
+++ catkin_ws/src/deepfind_package/scripts/SystemCom.py
@@ -6,32 +6,39 @@ from deepfind_package.msg import *
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
-sensorData = sensor_data()
+global imuData
+imuData = imu_data()
 
 #-----------------------Callback Functions-------------------#
 
 def imu_callback(data):
-	sensorData.imu = data
-#	print(sensorData.imu)
+	imuData = data
+	print(imuData)
 
 def lidar_callback(data):
-	sensorData.lidar = data
+	global lidarData
+	lidarData = data
 
 def encoder_callback(data):
-	sensorData.encoder = data
+	global encoderData
+	encoderData = data
 
 
 #----------------------Communication Set Up-----------------#
 def run():
-	global sensorData
-	sensorData = sensor_data()
+#	global sensorData
+#	sensorData = sensor_data()
 	rospy.init_node('system_communication')
 	global controlPub
 	controlPub = rospy.Publisher('cmd_vel', Twist, queue_size = 10)
 	imuSub = rospy.Subscriber('vn100_yaw', imu_data, imu_callback)
 	lidarSub = rospy.Subscriber('scan', LaserScan, lidar_callback)
 	encoderSub = rospy.Subscriber('encoder', encoders_data, encoder_callback)
-	print(sensorData)
+#	print(sensorData)
+#	sensorData = sensor_data()
+#	sensorData.imu = imuData
+#	sensorData.lidar = lidarData
+#	sensorData.encoder = encoderData
 	rospy.spin()
 
 #----------------------Publisher----------------------------#
@@ -40,10 +47,10 @@ def send_commnand(command):
 	controlPub.publish(command)
 
 
-#----------------------Subscriber---------------------------#
+#-----------------------------------------------------------#
 def get_sensor_data():
-	print(sensorData)
-	return sensorData
+	print(imuData)
+	return imuData
 
 if __name__ == '__main__':
 	run()
