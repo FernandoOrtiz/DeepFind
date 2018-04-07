f8f91564926ae553976d4972608b360603589999
Pedro J Fernandez <pedro.fernandez2@upr.edu>
2018-04-07 09:05:33 -0700

    Added deepfind_navigation package

--- catkin_ws/devel/share/deepfind_package/cmake/deepfind_package-msg-paths.cmake
+++ catkin_ws/devel/share/deepfind_package/cmake/deepfind_package-msg-paths.cmake
@@ -1,4 +1,4 @@
 # generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
 
-set(deepfind_package_MSG_INCLUDE_DIRS "/home/nvidia/DeepFind/catkin_ws/src/deepfind_package/msg")
+set(deepfind_package_MSG_INCLUDE_DIRS "/home/resorte/DeepFind/catkin_ws/src/deepfind_package/msg")
 set(deepfind_package_MSG_DEPENDENCIES sensor_msgs)
--- catkin_ws/devel/share/gennodejs/ros/deepfind_package/msg/_index.js
+++ catkin_ws/devel/share/gennodejs/ros/deepfind_package/msg/_index.js
@@ -1,14 +1,14 @@
 
 "use strict";
 
-let encoders_data = require('./encoders_data.js');
-let sensor_data = require('./sensor_data.js');
 let imu_data = require('./imu_data.js');
 let motor_command = require('./motor_command.js');
+let encoders_data = require('./encoders_data.js');
+let sensor_data = require('./sensor_data.js');
 
 module.exports = {
-  encoders_data: encoders_data,
-  sensor_data: sensor_data,
   imu_data: imu_data,
   motor_command: motor_command,
+  encoders_data: encoders_data,
+  sensor_data: sensor_data,
 };
