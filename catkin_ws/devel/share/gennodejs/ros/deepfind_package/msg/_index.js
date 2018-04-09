78175087d35398591ef9539c3eea8e4f225c47b8
Pedro <pedro.fernandez2@upr.edu>
2018-04-09 20:24:02 +0000

    Updated deepfind_distance

--- catkin_ws/devel/share/gennodejs/ros/deepfind_package/msg/_index.js
+++ catkin_ws/devel/share/gennodejs/ros/deepfind_package/msg/_index.js
@@ -1,18 +1,18 @@
 
 "use strict";
 
-let imu_data = require('./imu_data.js');
-let motor_command = require('./motor_command.js');
+let distance_traveled = require('./distance_traveled.js');
 let keyboard = require('./keyboard.js');
 let encoders_data = require('./encoders_data.js');
-let distance_traveled = require('./distance_traveled.js');
 let sensor_data = require('./sensor_data.js');
+let imu_data = require('./imu_data.js');
+let motor_command = require('./motor_command.js');
 
 module.exports = {
-  imu_data: imu_data,
-  motor_command: motor_command,
+  distance_traveled: distance_traveled,
   keyboard: keyboard,
   encoders_data: encoders_data,
-  distance_traveled: distance_traveled,
   sensor_data: sensor_data,
+  imu_data: imu_data,
+  motor_command: motor_command,
 };
