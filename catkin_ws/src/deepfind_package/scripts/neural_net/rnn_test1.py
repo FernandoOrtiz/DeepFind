cb135acd568583e587f28ea43a99b96e1f24f287
ortizfer <Sic@rio21>
2018-04-15 15:59:55 -0400

    added further implementation of neural net

--- catkin_ws/src/deepfind_package/scripts/neural_net/rnn_test1.py
+++ catkin_ws/src/deepfind_package/scripts/neural_net/rnn_test1.py
@@ -8,6 +8,7 @@ RNN Implementation
 #general purpose imports
 import pandas as pd 
 from matplotlib import pyplot
+import numpy as np
 
 #Neural Network Keras Imports
 from keras.models import Sequential
@@ -17,23 +18,39 @@ from keras.layers import Dropout
 
 #Set Training Data 
 data_set = pd.read_csv('')
-training_set = dataset_train.iloc[:, 1:2].values
+training_set = data_set.iloc[:, 1:2].values
 
 #data structure
 X=[]
 Y=[]
 for i in range(60, 1258):
-    X.append(training_set_scaled[i-60:i, 0])
-    Y.append(training_set_scaled[i, 0])
+    X.append(training_set[i-60:i, 0])
+    Y.append(training_set[i, 0])
 X, Y = np.array(X), np.array(Y)
 
 # Reshaping
 X = np.reshape(X, (X.shape[0], X.shape[1], 1))
 
-
 #Initialization and Creation of the RNN
 network = Sequential()
-network.add()
+network.add(LSTM(units = 50, return_sequences = True, input_shape = (X.shape[1], 1)))
+network.add(Dropout(0.5))
+
+# Adding a second LSTM layer and some Dropout regularisation
+network.add(LSTM(units = 50, return_sequences = True))
+network.add(Dropout(0.5))
+
+# Adding a third LSTM layer and some Dropout regularisation
+network.add(LSTM(units = 50, return_sequences = True))
+network.add(Dropout(0.5))
+
+# Adding a fourth LSTM layer and some Dropout regularisation
+network.add(LSTM(units = 50))
+network.add(Dropout(0.5))
+
+# Adding the output layer
+network.add(Dense(units = 1))
+
 
 #verify the history of the training
 network.compile(optimizer = 'adam', loss='logcosh', metrics=['accuracy'])
