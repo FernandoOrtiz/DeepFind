46fff20cc53e64390706089c083c544fc2484d44
giovanni2694 <giovanni.gardon@upr.edu>
2018-04-23 16:31:42 -0700

    now it works

--- catkin_ws/src/deepfind_package/scripts/power_monitor.py
+++ catkin_ws/src/deepfind_package/scripts/power_monitor.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 
+"""
+Power Monitor 
+The purpose of this file is to monitor the comouter's voltage and current input
+at all times and act accordingly in order to portect the hardware
+"""
+
 import rospy
 import os
 import time
 
+sudoPassword = 'nvidia'
+command = 'sudo shutdown now'
 
-
+#Monitors the voltage and current input
+#Shuts down Jetson when voltage reaches 11.5V
 def get_input_power():
 	with open("/sys/bus/i2c/devices/0-0040/iio_device/in_current0_input",'r') as i:
 		current = next(i)
@@ -18,13 +27,16 @@ def get_input_power():
 	voltage = int(line[0]+line[1])
 	voltageDecimal = int(line[3])
 	print("Voltage = " + str( voltage) + "." + str(voltageDecimal))
-	if voltage <= 11 and voltageDecimal < 6 and enable == 1:
-		print('hi')
-		#todo: save data
-		#os.system("rosnode kill -a")
-		os.system("sudo shutdown -h now")		
-    
+	if voltage <= 11 and voltageDecimal < 5 and enable == 1:
+		print('shutting down')
+		os.system('echo %s|sudo -S %s' % (sudoPassword, command))	
+	
+		
 
+    
+#Main function:
+#Responsible for starting the ros node 
+#and monitoring the power every 5 seconds
 if __name__ == '__main__':
 	rospy.init_node('power_monitor')
 	enable = 0
