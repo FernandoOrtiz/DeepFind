0ba6087ffead3f3308edf39084f2de3c19f9fecf
Fernando <fernando.ortiz10@upr.edu>
2018-04-23 22:43:01 -0400

    added implementation for neural net and tensorboard

--- Deep Learning/src/rnn_test2.py
+++ Deep Learning/src/rnn_test2.py
@@ -9,6 +9,7 @@ import pandas as pd
 from matplotlib import pyplot
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+from keras.callbacks import TensorBoard
 
 
 #Neural Network Keras Imports
@@ -19,37 +20,37 @@ from keras.layers import Dropout
 from keras.optimizers import Adam
 
 
+
 #The amount of time-steps the LSTM will look back at
 step = 20
-val_dat = 0.4
+val_dat = 0.15
 
 def setup_data(time_step):
     global X
     global Y
     global sc
     #Import the training set
-    training_set = pd.read_csv("../Datasets/Training_Data - Copy2.csv")
+    #train_dataset = pd.read_csv("../Datasets/Training_Data - Copy2.csv")
     #Obtain the test Data
-    test_set = pd.read_csv("../Datasets/Test_Data - Copy3.csv")
+    #test_dataset = pd.read_csv("../Datasets/Test_Data - Copy3.csv")
     #Merge both test and train to obtain initial values
-    dataset_total = pd.concat((training_set, test_set), axis = 0)
-    
-    
+    #dataset_total = pd.concat((train_dataset, test_dataset), axis = 0)
+    dataset_total = pd.read_csv("../Datasets/30MinuteRun-M1.csv")
+    dataset_total = dataset_total.as_matrix()
     #Reshappe the inpputt so it fits the neural network
     X = []
     for i in range(1, time_step):
-        X.append(pd.concat((pd.DataFrame(np.zeros((step-i, dataset_total.shape[1]-2))), pd.DataFrame(dataset_total.as_matrix()[0:i, 2:])), axis = 0).as_matrix())
+        X.append(np.concatenate((np.zeros((time_step-i, dataset_total.shape[1]-2)), dataset_total[0:i, 2:]), axis = 0))
     
     
     for i in range(time_step-1, int(dataset_total.size/dataset_total.shape[1])):
-        X.append(dataset_total.as_matrix()[i-step+1 : i+1 , 2:])
+        X.append(dataset_total[i-time_step+1 : i+1 , 2:])
     X = np.array(X)
     
     
     #Extract the output of the neural network
     #y = dataset_total.iloc[time_step-1:,0:2]
-    Y = dataset_total.iloc[0:,0:2]
-    Y = Y.as_matrix()
+    Y = dataset_total[0:,0:2]
     #Feature Scaling
     sc = MinMaxScaler(feature_range = (-1,1))
     Y = sc.fit_transform(Y)
@@ -64,38 +65,39 @@ setup_data(step)
 #Initialization and Creation of the RNN
 
 network = Sequential()
-network.add(LSTM(units = 15, return_sequences = True, input_shape = (X.shape[1], X.shape[2])))
-network.add(Dropout(0.02))
-network.add(Dense(units = 15, activation = 'tanh'))
-
-# Adding a second LSTM layer and some Dropout regularisation
-network.add(LSTM(units = 15, return_sequences = True))
-network.add(Dropout(0.02))
-network.add(Dense(units = 15, activation = 'tanh'))
-
-# Adding a second LSTM layer and some Dropout regularisation
-#network.add(LSTM(units = 5, return_sequences = True))
-#network.add(Dropout(0.5))
-#network.add(Dense(units = 20, activation = 'tanh'))
-
-# Adding a third LSTM layer and some Dropout regularisation
-#network.add(LSTM(units = 10, return_sequences = True))
+network.add(LSTM(units = 30, return_sequences = True, input_shape = (X.shape[1], X.shape[2]),dropout= 0,recurrent_dropout=0))
 #network.add(Dropout(0.2))
 #network.add(Dense(units = 10, activation = 'tanh'))
 
-# Adding a fourth LSTM layer and some Dropout regularisation
-network.add(LSTM(units = 15))
-network.add(Dropout(0.02))
+# Adding a second LSTM layer and some Dropout regularisation
+#network.add(LSTM(units = 20, return_sequences = True,dropout= 0.2,recurrent_dropout=0.2))
+#network.add(Dropout(0.2))
+#network.add(Dense(units = 20, activation = 'tanh'))
 
-network.add(Dense(units = 15, activation = 'tanh'))
+# Adding a second LSTM layer and some Dropout regularisation
+#network.add(LSTM(units = 20, return_sequences = True,dropout= 0.2,recurrent_dropout=0.2))
+#network.add(Dropout(0.2))
+#network.add(Dense(units = 20, activation = 'tanh'))
+
+# Adding a third LSTM layer and some Dropout regularisation
+#network.add(LSTM(units = 20, return_sequences = True,dropout= 0.2,recurrent_dropout=0.2))
+#network.add(Dropout(0.2))
+#network.add(Dense(units = 20, activation = 'tanh'))
+
+# Adding a fourth LSTM layer and some Dropout regularisation
+network.add(LSTM(units = 30,dropout= 0,recurrent_dropout=0))
+#network.add(Dropout(0.2))
+
+#network.add(Dense(units = 10, activation = 'tanh'))
 
 # Adding the output layer
 network.add(Dense(units = 2, activation = 'tanh'))
 
 
 #Validate the neural net
-network.compile(optimizer = Adam(lr=0.0002), loss='logcosh', metrics=['accuracy'])
-history = network.fit(X, Y, validation_split=val_dat, epochs=150, batch_size = 32) 
+network.compile(optimizer = Adam(lr=0.002), loss='logcosh', metrics=['accuracy'])
+call_back = TensorBoard(log_dir='../',write_graph=True)
+history = network.fit(X, Y, validation_split=val_dat, epochs=150, batch_size = 64, callbacks = call_back) 
 #print(history.history['loss'])
 #print(history.history['acc'])
 #print(history.history['val_loss'])
@@ -135,16 +137,18 @@ prediction = sc.inverse_transform(prediction)
 expected_outcome = sc.inverse_transform(Y[slice_index:,:])
 
    
-pyplot.plot(prediction[0:,0:1])
+
 pyplot.plot(expected_outcome[0:, 0:1])
+pyplot.plot(prediction[0:,0:1])
 pyplot.title('predictions vs expected outcome')
 pyplot.ylabel('measurement')
 pyplot.xlabel('meters')
 pyplot.legend(['prediction', 'expected outcome'], loc='upper right')
 pyplot.show()
 
-pyplot.plot(prediction[0:,1:2])
+
 pyplot.plot(expected_outcome[0:, 1:2])
+pyplot.plot(prediction[0:,1:2])
 pyplot.title('predictions vs expected outcome')
 pyplot.ylabel('measurement')
 pyplot.xlabel('meters')
@@ -156,4 +160,4 @@ setup_data(step)
 
 
 #Save the trained model
-#network.save('LSTM-68%accuracy-Good-Graphs-Fer.h5py')
+#network.save('LSTM-70%accuracy-Good-Predictions-Fer.h5py')
