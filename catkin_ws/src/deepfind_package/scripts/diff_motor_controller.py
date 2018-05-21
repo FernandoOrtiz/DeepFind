24100d53d362fead3fa7c1d3f8e99533f690c5e8
giovanni2694 <giovanni.gardon@upr.edu>
2018-05-20 21:21:10 -0700

    Commneted code

--- catkin_ws/src/deepfind_package/scripts/diff_motor_controller.py
+++ catkin_ws/src/deepfind_package/scripts/diff_motor_controller.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 
+"""
+#The differential controller script is responsible sending velocity commands
+#to the Arduino that controls de DC Motors. Here the Twist message coming from 
+#the teleop device is converted into MotorCommand message used by the Arduino.
+"""
+
 import rospy
 from deepfind_package.msg import MotorCommand
 from geometry_msgs.msg import Twist
 
+# Motor global variables 
 global dict
 dict = {'MOTOR_CAP': 40, 'FORWARD': 0, 'BACKWARD': 1}
 
-
+# Converts incoming Twist message to Motorommand message and
+# sends the command to the Arduino.
 def diff_control_callback(twistData):
   command = MotorCommand()
   command.leftMotorPower = int((twistData.linear.x + twistData.angular.z) * dict['MOTOR_CAP'])
@@ -29,7 +37,8 @@ def diff_control_callback(twistData):
   pub = rospy.Publisher('motor_speed', MotorCommand, queue_size = 10)
   pub.publish(command)
 
-
+# Main function, waits for new Twist messages to arrive
+# Creates node and subscriber
 def diff_controller_listener():
   rospy.init_node('differential_controller')
   rospy.Subscriber('cmd_vel', Twist, diff_control_callback)
@@ -38,5 +47,4 @@ def diff_controller_listener():
 if __name__ == '__main__':
     diff_controller_listener()
 
-#export ROS_MASTER_URI=http://192.168.43.212:11311
 
