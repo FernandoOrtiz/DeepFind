bc81986157b83ff5315f01952e4c4c071aa6be9a
giovanni2694 <giovanni.gardon@upr.edu>
2018-05-20 21:43:52 -0700

    bug fixes

--- catkin_ws/src/deepfind_package/scripts/power_monitor.py
+++ catkin_ws/src/deepfind_package/scripts/power_monitor.py
@@ -9,9 +9,12 @@ at all times and act accordingly in order to portect the hardware
 import rospy
 import os
 import time
+from std_msgs.msg import String
 
 sudoPassword = 'nvidia'
 command = 'sudo shutdown now'
+lastVoltage = 11
+lastVoltageDecimal = 6
 
 #Monitors the voltage and current input
 #Shuts down Jetson when voltage reaches 11.5V
@@ -26,10 +29,19 @@ def get_input_power():
 		line = next(i)
 	voltage = int(line[0]+line[1])
 	voltageDecimal = int(line[3])
-	print("Voltage = " + str( voltage) + "." + str(voltageDecimal))
-	if voltage <= 10 and voltageDecimal < 5 and enable == 1:
-		print('shutting down')
-		os.system('echo %s|sudo -S %s' % (sudoPassword, command))	
+	#print("Voltage = " + str( voltage) + "." + str(voltageDecimal))
+
+	power_pub.publish("Voltage = " + str(voltage + voltageDecimal) + "V		" + "Current = " + str(current[0]) + "A 	" + "Power = " + str(current[0]*voltage) + "W" )
+	
+
+	if voltage <= 10 and voltageDecimal < 2 and enable == 1:
+		if (lastVoltage <= 10 and lastVoltageDecimal < 2):
+			print('shutting down')
+			os.system('echo %s|sudo -S %s' % (sudoPassword, command))	
+
+
+	lastVoltage = voltage
+	lastVoltageDecimal = voltageDecimal
 	
 		
 
@@ -39,6 +51,8 @@ def get_input_power():
 #and monitoring the power every 5 seconds
 if __name__ == '__main__':
 	rospy.init_node('power_monitor')
+	global power_pub
+	power_pub = rospy.Publisher('power_consumption', String, queue_size = 1000)
 	enable = 0
 	while(1):
 		get_input_power()
